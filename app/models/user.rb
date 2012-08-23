0a972c1354be0de32c0ea9234a35da97de2b094e
Smit Shah <who828@gmail.com>
2012-08-23 11:15:13 +0530

    Added validation for unique emails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :email, :name
   validates_email_format_of :email
+  validates_uniqueness_of :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,10 @@ describe User do
   it { should respond_to(:password_confirmation) }
 
   context "validations" do
+    subject { FactoryGirl.create(:user) }
     it { should validate_presence_of(:email)}
     it { should validate_presence_of(:name)}
-    it { should validate_presence_of(:password_digest)}    
+    it { should validate_presence_of(:password_digest)}
+    it { should validate_uniqueness_of(:email) }
   end
 end
