beac37d27e4960177a5079df0ee50a212e480a3e
Smit Shah <who828@gmail.com>
2012-08-28 16:34:39 +0530

    Added conditional validations for admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,16 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   validates_presence_of :email, :name
+  validates_presence_of :organization_id, :unless => :admin?
   validates_email_format_of :email
   validates_uniqueness_of :email
   belongs_to :organization
 
   ROLES = %w(admin cso_admin)
+
+  private
+
+  def admin?
+    role == 'admin'
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,10 +8,20 @@ describe User do
   it { should belong_to(:organization) }
 
   context "validations" do
-    subject { FactoryGirl.create(:user) }
+    subject { FactoryGirl.create(:user, :organization => FactoryGirl.create(:organization)) }
     it { should validate_presence_of(:email)}
     it { should validate_presence_of(:name)}
     it { should validate_presence_of(:password_digest)}
     it { should validate_uniqueness_of(:email) }
+
+    context "user is not an admin" do
+      it { should validate_presence_of(:organization_id) }
+    end
+
+    context "user is an admin" do
+      subject { FactoryGirl.create(:user, :role => 'admin')}
+      it { should_not validate_presence_of(:organization_id) }
+    end
+
   end
 end
