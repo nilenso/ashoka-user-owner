ad4e5760fc6563ef68368e152b0cdee2a4ab6c60
Srihari Sriraman <techie@visishta.net>
2012-08-30 12:03:22 +0530

    added sendgrid credentials

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,14 @@
 class UserMailer < ActionMailer::Base
-  default from: "admin@user_owner.app"
+  # include SendGrid
+  default from: "friendly_robot@user-owner-staging.herokuapp.com"
 
   def approval_mail(user)
     @user = user
-    mail(:to => user.email, :from => 'srihari@c42.in', :subject => "Welcome to user owner")
+    mail(:to => user.email, :subject => "Welcome to user owner")
   end
 
   def rejection_mail(user)
     @user = user
-    mail(:to => user.email, :from => 'srihari@c42.in', :subject => "Your organization has not been approved")
+    mail(:to => user.email, :subject => "Your organization has not been approved")
   end
 end
