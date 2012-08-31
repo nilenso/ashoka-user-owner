511678da003540d362610b2b50d58a03ab24357d
Smit Shah <who828@gmail.com>
2012-08-31 18:43:41 +0530

    Fixed the build, forgot to add subject to test uniqueness

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Organization do
+  subject { FactoryGirl.create(:organization) }
   it { should have_many(:users) }
   it { should accept_nested_attributes_for(:users) }
   it { should validate_presence_of(:name) }
