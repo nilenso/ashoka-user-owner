bcf8563b03895af995df37f0ac0ae5081b68d9ed
Nivedita <nivedita@c42.in>
2012-09-04 14:33:53 +0530

    i18ned thge flash messges in password resets

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,7 +10,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.reset_password(params[:user][:password],params[:user][:password_confirmation])
       reset_session
-      redirect_to root_path, :notice => "Password has been reset!"
+      redirect_to root_path, :notice => t(".password_reset")
     else
       render :edit
     end
