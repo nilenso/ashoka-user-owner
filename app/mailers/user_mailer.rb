8590c12ee1620ef8fd4811cad2fb6a3d9535a75d
Timothy Andrew <mail@timothyandrew.net>
2012-09-07 12:09:42 +0530

    OrganizationsController passes it's default_locale to the UserMailer.
    Modified specs to reflect the new method signatures.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ActionMailer::Base
   default from: "\"User Owner Robot\" <friendly_robot@user-owner-staging.herokuapp.com>"
 
-  def approval_mail(user)
+  def approval_mail(user, locale)
     @user = user
     mail(:to => user.email, :subject => "Welcome to user owner")
   end
 
-  def rejection_mail(user, message=nil)
+  def rejection_mail(user, locale, message=nil)
     @user = user
     @message = message
     mail(:to => user.email, :subject => "Your organization has not been approved")
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,32 +4,32 @@ describe UserMailer do
   it "sends an approval mail to the user" do
     org = FactoryGirl.create(:organization)
     user = FactoryGirl.create(:user, :email => "bar@foo.com", :organization => org)
-    lambda { UserMailer.approval_mail(user) }.should_not raise_error
-    lambda { UserMailer.approval_mail(user).deliver }.should_not raise_error
-    lambda { UserMailer.approval_mail(user).deliver }.should change(ActionMailer::Base.deliveries,:size).by(1)
+    lambda { UserMailer.approval_mail(user, 'en') }.should_not raise_error
+    lambda { UserMailer.approval_mail(user, 'en').deliver }.should_not raise_error
+    lambda { UserMailer.approval_mail(user, 'en').deliver }.should change(ActionMailer::Base.deliveries,:size).by(1)
   end
 
   it "sends a rejection mail to the user" do
     org = FactoryGirl.create(:organization)
     user = FactoryGirl.create(:user, :email => "baz@foo.com", :organization => org)
-    lambda { UserMailer.rejection_mail(user) }.should_not raise_error
-    lambda { UserMailer.rejection_mail(user).deliver }.should_not raise_error
-    lambda { UserMailer.rejection_mail(user).deliver }.should change(ActionMailer::Base.deliveries,:size).by(1)
+    lambda { UserMailer.rejection_mail(user, 'en') }.should_not raise_error
+    lambda { UserMailer.rejection_mail(user, 'en').deliver }.should_not raise_error
+    lambda { UserMailer.rejection_mail(user, 'en').deliver }.should change(ActionMailer::Base.deliveries,:size).by(1)
   end
 
   it "should be delivered to the user's email" do
     org = FactoryGirl.create(:organization)
     user = FactoryGirl.create(:user, :email => "baz@foo.com", :organization => org)
-    email = UserMailer.approval_mail(user)
+    email = UserMailer.approval_mail(user, 'en')
     email.should deliver_to('baz@foo.com')
   end
 
   it "should have the user's name in the body" do
     org = FactoryGirl.create(:organization)
     user = FactoryGirl.create(:user, :email => "baz@foo.com", :organization => org)
-    email = UserMailer.approval_mail(user)
+    email = UserMailer.approval_mail(user, 'en')
     email.should have_body_text(user.name)
-    email = UserMailer.rejection_mail(user)
+    email = UserMailer.rejection_mail(user, 'en')
     email.should have_body_text(user.name)
   end
 
@@ -37,23 +37,23 @@ describe UserMailer do
   it "should have the organization's name in the body" do
     org = FactoryGirl.create(:organization)
     user = FactoryGirl.create(:user, :email => "baz@foo.com", :organization => org)
-    email = UserMailer.approval_mail(user)
+    email = UserMailer.approval_mail(user, 'en')
     email.should have_body_text(org.name)
-    email = UserMailer.rejection_mail(user)
+    email = UserMailer.rejection_mail(user, 'en')
     email.should have_body_text(org.name)
   end
 
   it "should deliver from an email at the same domain" do
     org = FactoryGirl.create(:organization)
     user = FactoryGirl.create(:user, :email => "baz@foo.com", :organization => org)
-    email = UserMailer.approval_mail(user)
+    email = UserMailer.approval_mail(user, 'en')
     email.should deliver_from("\"User Owner Robot\" <friendly_robot@user-owner-staging.herokuapp.com>")
   end
 
   it "should include the rejection message in the body" do
     org = FactoryGirl.create(:organization)
     user = FactoryGirl.create(:user, :email => "baz@foo.com", :organization => org)
-    email = UserMailer.rejection_mail(user, "someuniquemessage")
+    email = UserMailer.rejection_mail(user, 'en', "someuniquemessage")
     email.should have_body_text("someuniquemessage")
   end
 
