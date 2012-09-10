43d82b25effebc30bba8d1c4f57c6546142a96db
Timothy Andrew <mail@timothyandrew.net>
2012-09-10 18:44:40 +0530

    I18n for password_reset success flash message.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,7 +10,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.reset_password(params[:user][:password],params[:user][:password_confirmation])
       reset_session
-      redirect_to root_path, :notice => t(".password_reset")
+      redirect_to root_path, :notice => t("password_resets.success")
     else
       render :edit
     end
