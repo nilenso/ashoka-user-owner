053873eb67fd505640ae47639cff141ec28ade3f
Nivedita <nivedita@c42.in>
2012-10-05 12:14:21 +0530

    UsersController#create depends on the model's default values for role & status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     @user.organization_id = params[:organization_id]
-    @user.role = 'user'
-    @user.status = User::Status::PENDING
     @user.generate_password
     if @user.save
       flash[:notice] = t "users.create.user_created_successfully"
