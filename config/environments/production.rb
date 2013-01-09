2694f89a18028406161ab62e4e4d26e9e1bec3bc
Nivedita <nivedita@c42.in>
2013-01-09 16:44:20 +0530

    Added cloud front asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ UserService::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//d1wrzpb6e06evy.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   #config.assets.precompile += %w( login_page.css register_page.css admin_page.css user_list.css )
