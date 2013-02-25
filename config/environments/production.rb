98fe41aaa9d53b5415fe9e5ae538b551cdd59bb9
Nivedita <nivedita@c42.in>
2013-02-25 17:23:27 +0530

    Removed cloudfront.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ UserService::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "//d12coq2i6n6lst.cloudfront.net"
+  # config.action_controller.asset_host = "//d12coq2i6n6lst.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   #config.assets.precompile += %w( login_page.css register_page.css admin_page.css user_list.css )
