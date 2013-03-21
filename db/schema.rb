3beff184788795772382bf26fa9e854e8a441ffd
Nivedita <nivedita@c42.in>
2013-03-21 12:43:55 +0530

    Updated the schema.

--- db/schema.rb
+++ db/schema.rb
@@ -13,60 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130319094959) do
 
-  create_table "answers", :force => true do |t|
-    t.text     "content"
-    t.integer  "question_id"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.integer  "response_id"
-    t.string   "photo"
-    t.string   "photo_secure_token"
-    t.string   "photo_tmp"
-    t.integer  "record_id"
-  end
-
-  add_index "answers", ["question_id"], :name => "index_answers_on_question_id"
-  add_index "answers", ["response_id"], :name => "index_answers_on_response_id"
-
-  create_table "categories", :force => true do |t|
-    t.integer  "category_id"
-    t.text     "content"
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
-    t.integer  "survey_id"
-    t.integer  "order_number"
-    t.integer  "parent_id"
-    t.string   "type"
-    t.boolean  "mandatory",    :default => false
-  end
-
-  add_index "categories", ["category_id"], :name => "index_categories_on_category_id"
-
-  create_table "choices", :force => true do |t|
-    t.integer  "answer_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "option_id"
-  end
-
-  add_index "choices", ["answer_id"], :name => "index_choices_on_answer_id"
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
   create_table "oauth_access_grants", :force => true do |t|
     t.integer  "resource_owner_id", :null => false
     t.integer  "application_id",    :null => false
@@ -106,16 +52,6 @@ ActiveRecord::Schema.define(:version => 20130319094959) do
 
   add_index "oauth_applications", ["uid"], :name => "index_oauth_applications_on_uid", :unique => true
 
-  create_table "options", :force => true do |t|
-    t.string   "content"
-    t.integer  "question_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.integer  "order_number"
-  end
-
-  add_index "options", ["question_id"], :name => "index_options_on_question_id"
-
   create_table "organizations", :force => true do |t|
     t.string   "name"
     t.datetime "created_at",                           :null => false
@@ -124,84 +60,6 @@ ActiveRecord::Schema.define(:version => 20130319094959) do
     t.string   "default_locale", :default => "en"
     t.string   "org_type"
   end
-
-  create_table "participating_organizations", :force => true do |t|
-    t.integer  "survey_id"
-    t.integer  "organization_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
-  create_table "questions", :force => true do |t|
-    t.text     "content"
-    t.integer  "survey_id"
-    t.datetime "created_at",                                         :null => false
-    t.datetime "updated_at",                                         :null => false
-    t.boolean  "mandatory",                       :default => false
-    t.integer  "max_length",         :limit => 8
-    t.string   "type"
-    t.integer  "max_value",          :limit => 8
-    t.integer  "min_value"
-    t.integer  "order_number"
-    t.integer  "parent_id"
-    t.boolean  "identifier",                      :default => false
-    t.integer  "category_id"
-    t.string   "image"
-    t.string   "photo_secure_token"
-    t.string   "image_tmp"
-  end
-
-  add_index "questions", ["survey_id"], :name => "index_questions_on_survey_id"
-
-  create_table "records", :force => true do |t|
-    t.integer  "category_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.integer  "response_id"
-  end
-
-  create_table "responses", :force => true do |t|
-    t.integer  "survey_id"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.integer  "user_id"
-    t.integer  "organization_id"
-    t.string   "status",          :default => "incomplete"
-    t.string   "session_token"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.string   "location"
-    t.string   "ip_address"
-    t.string   "mobile_id"
-    t.string   "state"
-    t.text     "comment"
-  end
-
-  add_index "responses", ["organization_id"], :name => "index_responses_on_organization_id"
-  add_index "responses", ["survey_id"], :name => "index_responses_on_survey_id"
-
-  create_table "survey_users", :force => true do |t|
-    t.integer  "survey_id"
-    t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "surveys", :force => true do |t|
-    t.string   "name"
-    t.date     "expiry_date"
-    t.text     "description"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
-    t.boolean  "finalized",       :default => false
-    t.integer  "organization_id"
-    t.boolean  "public",          :default => false
-    t.string   "auth_key"
-    t.date     "published_on"
-    t.boolean  "archived",        :default => false
-  end
-
-  add_index "surveys", ["organization_id"], :name => "index_surveys_on_organization_id"
 
   create_table "users", :force => true do |t|
     t.string   "name"
