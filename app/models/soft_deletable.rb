1848425097cee82edc2524f8f66f3895400dde79
Timothy Andrew <mail@timothyandrew.net>
2013-06-19 14:14:02 +0530

    [#51542731] A SoftDeletable model excludes deleted elements by default.

--- app/models/soft_deletable.rb
+++ app/models/soft_deletable.rb
@@ -1,4 +1,13 @@
+require 'active_support/concern'
+
 module SoftDeletable
+  extend ActiveSupport::Concern
+
+  included do
+    scope :not_deleted, where('deleted_at IS NULL')
+    default_scope where('deleted_at IS NULL')
+  end
+
   def soft_delete
     self.deleted_at = Date.today
     self.save
--- spec/support/shared_examples_for_soft_deletable.rb
+++ spec/support/shared_examples_for_soft_deletable.rb
@@ -8,4 +8,17 @@ shared_examples "a soft-deletable element" do
       element.deleted_at.should == date
     end
   end
+
+  context "scopes" do
+    let!(:element) { FactoryGirl.create(described_class) }
+    let!(:deleted_element) { FactoryGirl.create(described_class, :deleted_at => 5.days.ago) }
+
+    it "finds all the elements that are not soft-deleted" do
+      described_class.not_deleted.should == [element]
+    end
+
+    it "excludes soft-deleted elements by default" do
+      described_class.all.should == [element]
+    end
+  end
 end
