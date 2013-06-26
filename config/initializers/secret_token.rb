31ed0c07f3f6372556344a70c096cddba8be851e
Jithu Gopal <jithug87@gmail.com>
2013-06-26 13:36:21 +0530

    [#52366577] moving secret token out of VCS

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-UserService::Application.config.secret_token = 'de4b02bb408be09d1577b8359b722fa99c5a7859f0d87910f428d33db63464d6bd92858b7dd1c12e533f6e187e05f2cbc4eb44c6680c62bef9c7ac54d0281716'
+UserService::Application.config.secret_token = ENV['SECRET_TOKEN']
