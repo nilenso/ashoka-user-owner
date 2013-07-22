a6901478a002d5767fc03f586635f0f2acf7d1f6
Timothy Andrew <mail@timothyandrew.net>
2013-07-22 11:02:20 +0530

    [#53281509] Be explicit in the abilities for an Organization.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,7 +11,9 @@ class Ability
       can :manage, :document
     when 'cso_admin'
       can :manage, User, :organization_id => user.organization_id
-      can :manage, Organization, :id => user.organization_id
+      can :read, Organization, :id => user.organization_id
+      can :update, Organization, :id => user.organization_id
+      can :destroy, Organization, :id => user.organization_id
     when nil
       can :create, Organization
     else
